68d42b3d3b7e192ff18eb9158ae376ea6e8acc86
lodi2407 <lodi2407@laposte.net>
2022-10-21 14:37:07 +0200

    WIP accueil sorties

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -7,7 +7,7 @@ use App\Entity\Participant;
 use App\Entity\Sortie;
 use App\Form\CreateSortieFormType;
 use App\Repository\EtatRepository;
-use App\Repository\LieuRepository;
+use App\Repository\SortieRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,16 +17,16 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/sortie', name: 'app_sortie')]
 class SortieController extends AbstractController
 {
-    #[Route('/', name: '_list')]
-    public function listSorties(): Response
+    #[Route('/accueil/{id}', name: '_list')]
+    public function listSorties(Request $request, Participant $user, SortieRepository $sortieRepository, EntityManagerInterface $entityManager): Response
     {
-        return $this->render('sortie/sortie.html.twig', [
-            'controller_name' => 'SortieController',
-        ]);
+        $sorties = $sortieRepository->findBy([], ['dateHeureDebut' => 'ASC']);
+
+        return $this->render('sortie/sortie.html.twig', compact('sorties'));
     }
 
     #[Route('/create/{id}', name: '_create')]
-    public function createSortie(Participant $user, Request $request, EntityManagerInterface $entityManager, EtatRepository $etatRepository, LieuRepository $lieuRepository): Response
+    public function createSortie(Participant $user, Request $request, EntityManagerInterface $entityManager, EtatRepository $etatRepository): Response
     {
         $sortie = new Sortie();
         $sortie->setOrganisateur($user);
